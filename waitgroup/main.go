f26f369322a41c6fcc3c3a0c8cb01ad0a7f53d02
Darren Kennedy <darrenk@dropbox.com>
2022-09-15 22:35:33 -0600

    Bug fixes and add verification

--- waitgroup/main.go
+++ waitgroup/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
 const nPages = 100
-const maxParsers = 20
+const nParsers = 20
 
 type pageLinks struct {
 	Page  int
@@ -18,49 +19,40 @@ type pageLinks struct {
 
 type pageParser struct {
 	pages    chan *pageLinks
-	count    atomic.Int32
+	wg       sync.WaitGroup
 	queueLen atomic.Int32
-	done     chan bool
 }
 
 func makeParser() *pageParser {
 	parser := new(pageParser)
-	parser.pages = make(chan *pageLinks)
-	parser.done = make(chan bool)
+	parser.pages = make(chan *pageLinks, 1)
+	parser.wg = sync.WaitGroup{}
 	return parser
 }
 
 func crawl(rootPage *pageLinks) {
 	parser := makeParser()
 	parser.sendPage(rootPage)
-	parser.startParser()
-	<-parser.done
-	close(parser.done)
+	for i := 0; i < nParsers; i++ {
+		parser.wg.Add(1)
+		go parser.parse()
+	}
+	parser.wg.Wait()
 	close(parser.pages)
 }
 
-func (p *pageParser) sendPage(toParse *pageLinks) bool {
+func (p *pageParser) sendPage(toParse *pageLinks) {
 	if !toParse.Seen {
 		p.queueLen.Add(1)
 		// Send on a separate goroutine so we don't block
 		go func() {
 			p.pages <- toParse
 		}()
-		return true
-	}
-	return false
-}
-
-func (p *pageParser) startParser() {
-	if p.count.Load() < maxParsers {
-		// Don't know when the goroutine will start so increment parser count now
-		count := p.count.Add(1)
-		fmt.Printf("Started parser %v\n", count)
-		go p.parse()
 	}
 }
 
 func (p *pageParser) parse() {
+	defer p.wg.Done()
 	for p.queueLen.Load() > 0 {
 		select {
 		case page := <-p.pages:
@@ -72,19 +64,12 @@ func (p *pageParser) parse() {
 
 			fmt.Printf("Parsing %v\n", page.Page)
 			time.Sleep(time.Millisecond * 250)
-			for i, page := range page.Links {
-				// Not starting another parse goroutine for the first link as this goroutine can parse it
-				if p.sendPage(page) && i > 0 {
-					p.startParser()
-				}
+			for _, page := range page.Links {
+				p.sendPage(page)
 			}
 			p.queueLen.Add(-1)
 		default:
 		}
-	}
-
-	if p.count.Add(-1) == 0 {
-		p.done <- true
 	}
 }
 
